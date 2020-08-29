a2d656f54b98e354ff5525c9f66342f0ebf814e1
Diana Eskina <diana.s.eskina@yandex.ru>
2020-08-29 18:04:50 +0300

    Scope singleton(by default)

--- Eskina/spring-app1/src/main/java/ru/eskina/springcourse/TestSpring.java
+++ Eskina/spring-app1/src/main/java/ru/eskina/springcourse/TestSpring.java
@@ -9,11 +9,14 @@ public class TestSpring {
         System.out.println(testBean.getName());
         MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        //
+        //test reference is the same(singleton bean scope)
         System.out.println(firstMusicPlayer==secondMusicPlayer);
         firstMusicPlayer.playMusic();
         System.out.println(firstMusicPlayer.getName());
         System.out.println(firstMusicPlayer.getVolume());
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
         context.close();
     }
 }
