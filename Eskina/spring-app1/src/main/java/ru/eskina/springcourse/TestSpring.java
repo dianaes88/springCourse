33b77b377aef32218358cba5beee16e62b0a15bb
Diana Eskina <diana.s.eskina@yandex.ru>
2020-08-31 14:39:53 +0300

    Init/destroy  bean methods

--- Eskina/spring-app1/src/main/java/ru/eskina/springcourse/TestSpring.java
+++ Eskina/spring-app1/src/main/java/ru/eskina/springcourse/TestSpring.java
@@ -5,18 +5,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        //test reference is the same(singleton bean scope)
-        System.out.println(firstMusicPlayer==secondMusicPlayer);
-        firstMusicPlayer.playMusic();
-        System.out.println(firstMusicPlayer.getName());
-        System.out.println(firstMusicPlayer.getVolume());
-        firstMusicPlayer.setVolume(10);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+        ClassicalMusic classic = context.getBean("musicBeanClassic", ClassicalMusic.class);
+        System.out.println(classic.getSong());
+
+//        TestBean testBean = context.getBean("testBean", TestBean.class);
+//        System.out.println(testBean.getName());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        //test reference is the same(singleton bean scope)
+//        System.out.println(firstMusicPlayer==secondMusicPlayer);
+//        firstMusicPlayer.playMusic();
+//        System.out.println(firstMusicPlayer.getName());
+//        System.out.println(firstMusicPlayer.getVolume());
+//        firstMusicPlayer.setVolume(10);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
         context.close();
     }
 }
