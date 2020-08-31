614b98266882d63e5b27d2fb7ceea64c9047aca6
Diana Eskina <diana.s.eskina@yandex.ru>
2020-08-31 18:43:38 +0300

    Autowired tested

--- Eskina/spring-app1/src/main/java/ru/eskina/springcourse/MusicPlayer.java
+++ Eskina/spring-app1/src/main/java/ru/eskina/springcourse/MusicPlayer.java
@@ -1,50 +1,55 @@
 package ru.eskina.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class MusicPlayer {
-    private List<Music> playList = new ArrayList<>();
-    private String name;
-    private int volume;
-
-    public String getName() {
-        return name;
+//    private List<Music> playList = new ArrayList<>();
+    private ClassicalMusic music;
+//    private String name;
+//    private int volume;
+    @Autowired
+    public MusicPlayer(ClassicalMusic music) {
+        this.music = music;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    //IoC
-//    public MusicPlayer(Music music) {
-//        this.music = music;
-//    }
 
     public MusicPlayer() {
     }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getVolume() {
+//        return volume;
+//    }
+//
+//    public void setVolume(int volume) {
+//        this.volume = volume;
+//    }
 
-    public MusicPlayer(List<Music> playList) {
-        this.playList = playList;
-    }
 
-    public void setPlayList(List<Music> playList) {
-        this.playList = playList;
-    }
+//    public MusicPlayer(List<Music> playList) {
+//        this.playList = playList;
+//    }
+//
+//    public void setPlayList(List<Music> playList) {
+//        this.playList = playList;
+//    }
 
+//    public  void playMusic() {
+//        playList.forEach(music-> System.out.println(music.getSong()));
+//    }
     public  void playMusic() {
-        playList.forEach(music-> System.out.println(music.getSong()));
+        System.out.println("Playing " + music.getSong());
     }
-
-    /*public void setMusic(Music music) {
-        this.music = music;
-    }*/
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
 }
