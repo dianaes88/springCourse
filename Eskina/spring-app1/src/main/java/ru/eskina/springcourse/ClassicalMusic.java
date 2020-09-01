4f51d5a6ceb54aaf98a188c70167c9092e3355af
Diana Eskina <diana.s.eskina@yandex.ru>
2020-09-01 17:07:52 +0300

    @Scope("prototype") tested

--- Eskina/spring-app1/src/main/java/ru/eskina/springcourse/ClassicalMusic.java
+++ Eskina/spring-app1/src/main/java/ru/eskina/springcourse/ClassicalMusic.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
-@Scope("singleton")
+@Scope("prototype")
 public class ClassicalMusic implements Music {
     List<String> songs = new ArrayList<>();
 
