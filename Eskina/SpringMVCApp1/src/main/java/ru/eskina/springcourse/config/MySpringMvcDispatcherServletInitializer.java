eb6ec04ef93e16b43f345ec4c8dfe33db504261a
Diana Eskina <diana.s.eskina@yandex.ru>
2020-09-03 12:28:16 +0300

    First mvc app configured with java code(instead of web.xml and applicationContextMVC.xml)

---
+++ Eskina/SpringMVCApp1/src/main/java/ru/eskina/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package ru.eskina.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        //all requests from the user we send to dispatcher servlet
+        return new String[] {"/"};
+    }
+}
