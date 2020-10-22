f10a485840e35f779f7d81692938dee22004d1f5
Diana Eskina <diana.s.eskina@yandex.ru>
2020-10-22 12:55:45 +0300

    Not commited from spring-app, spring MVC app New app Blog with using spring boot

---
+++ Eskina/blog/blog/src/main/java/ru/springboot/learning/blog/controllers/MainController.java
@@ -0,0 +1,13 @@
+package ru.springboot.learning.blog.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+}
