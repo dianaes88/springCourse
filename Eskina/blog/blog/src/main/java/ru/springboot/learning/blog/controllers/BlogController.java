691c7d34ce016f3dec5bd6f8349d88f816aa0ab5
Diana Eskina <diana.s.eskina@yandex.ru>
2020-11-06 13:26:24 +0300

    Implementing form for adding new post to the blog.

--- Eskina/blog/blog/src/main/java/ru/springboot/learning/blog/controllers/BlogController.java
+++ Eskina/blog/blog/src/main/java/ru/springboot/learning/blog/controllers/BlogController.java
@@ -3,7 +3,7 @@ package ru.springboot.learning.blog.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.springboot.learning.blog.models.Post;
 import ru.springboot.learning.blog.repository.PostRepository;
 
@@ -18,4 +18,22 @@ public class BlogController {
         model.addAttribute("posts", posts);
         return "blog-main";
     }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String addPost(@ModelAttribute("post") Post post) {
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+    /*@PostMapping("/blog/add")
+    public String addPost(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }*/
+
 }
